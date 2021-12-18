47eaf63d0ec2f410b92327c3ad4274b7535e67f9
Pedro Marques da Silva <posilva@gmail.com>
2021-12-18 23:16:47 +0000

    Add imgtool

---
+++ imgtool/src/main.rs
@@ -0,0 +1,11 @@
+use std::env;
+use std::path::Path;
+
+##[derive(Debug)]
+fn main() {
+    let image_path = env::args().skip(1).next().unwrap();
+    let path = Path::new(&image_path);
+    let img = image::open(path).unwrap();
+    let rotated = img.rotate270();
+    rotated.save(path).unwrap();
+}
